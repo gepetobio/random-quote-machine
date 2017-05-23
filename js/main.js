eb9d3c0cb9eb0b5563004a9d26a6d2c44b62f8c8
Gledsley Muller <gepetobio@gmail.com>
2017-05-24 00:33:34 +0100

    fixed https call instead of http

--- js/main.js
+++ js/main.js
@@ -1,1 +1,1 @@
-!function(t){"use strict";function n(){t.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",function(n){t("blockquote").html(n[0].content+"<p>— "+n[0].title+"</p>")})}n(),t(".js-new-quote").on("click",function(){n()})}(jQuery);
+!function(t){"use strict";function n(){t.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",function(n){t("blockquote").html(n[0].content+"<p>— "+n[0].title+"</p>")})}n(),t(".js-new-quote").on("click",function(){n()})}(jQuery);
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
   'use strict';
 
   function getQuote() {
-    $.getJSON('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=', function(data) {
+    $.getJSON('https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=', function(data) {
       $('blockquote').html(data[0].content + '<p>— ' + data[0].title + '</p>');
     });
   }
