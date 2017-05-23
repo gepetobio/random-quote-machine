f552a271e1785fcaa25df23f70b6c7bcf8175593
Gledsley Muller <gepetobio@gmail.com>
2017-05-24 00:30:27 +0100

    initial js call to fetch and generate new quotes

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # random-quote-machine
+
+Application can be seen on [https://gepetobio.github.io/random-quote-machine/](https://gepetobio.github.io/random-quote-machine/)
+
+## TODO
+
+* style the page
+* refactor html to be more semantic
+* add a button to tweet out a quote
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 (function($){
   'use strict';
 
-  $('body').addClass('temp'); //just a placeholder
+  function getQuote() {
+    $.getJSON('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=', function(data) {
+      $('blockquote').html(data[0].content + '<p>— ' + data[0].title + '</p>');
+    });
+  }
+
+  getQuote();
+
+  $('.js-new-quote').on('click', function() {
+    getQuote();
+  });
 }(jQuery));
