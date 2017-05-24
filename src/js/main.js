2b1f9ef28e2ff4a0f57dab361bb3356c64914610
Gledsley Muller <gepetobio@gmail.com>
2017-05-25 00:56:50 +0100

    initial styles and included first version of tweet button

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
   function getQuote() {
     $.getJSON('https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=', function(data) {
       $('blockquote').html(data[0].content + '<p>— ' + data[0].title + '</p>');
+      $('.js-tweet-btn').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent(data[0].content));
     });
   }
 
