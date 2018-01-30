dc99e607f53adba12dc74480444dd64084678047
Gledsley Muller <gledsley@energyhousedigital.com>
2018-01-30 17:59:34 -0200

    refactored jquery version

--- jquery/js/main.js
+++ jquery/js/main.js
@@ -1,1 +1,45 @@
-!function(t){"use strict";function e(){t.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=",function(e){t("blockquote").html(e[0].content+"<p>— "+e[0].title+"</p>"),t(".js-tweet-btn").attr("href","https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="+encodeURIComponent(e[0].content))})}e(),t(".js-new-quote").on("click",function(){e()})}(jQuery);
+(function ($) {
+
+  const apiURL = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+  const $newQuoteBtn = $('#newQuoteBtn');
+  const twitterURL = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp,javascript&text=';
+
+  const decodeHtmlEntity = str => {
+    return str.replace(/&#(\d+);/g, function (match, dec) {
+      return String.fromCharCode(dec);
+    });
+  };
+
+  const removeHTMLTags = str => str.replace(/<\/?[^>]+>/gi, '');
+
+  const changeTweet = (quote, author) => {
+    document.querySelector('.share .btn').href = twitterURL + encodeURIComponent('"' + quote.trim() + '" ' + author);
+  };
+
+  const makeRequest = (type, url, callback) => {
+    $.ajax({
+      url,
+      method: type
+    }).then(function(data) {
+      console.log(data);
+      callback(data);
+    });
+  };
+
+  const setNewQuote = data => {
+    const json = data;
+    const newQuote = decodeHtmlEntity(removeHTMLTags(json[0].content.trim()))
+    const newAuthor = json[0].title.trim();
+
+    $('.quote p').text(newQuote);
+    $('.citation span').text(newAuthor);
+
+    changeTweet(newQuote, newAuthor);
+  }
+
+  $newQuoteBtn.on('click', function() {
+    makeRequest('GET', apiURL, setNewQuote);
+  });
+
+  makeRequest('GET', apiURL, setNewQuote);
+}(jQuery));
