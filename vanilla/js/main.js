53df2028599aa3a8b453e01f800303e7a367de50
Gledsley Muller <gledsley@energyhousedigital.com>
2018-01-30 17:59:05 -0200

    added vanilla in a iife

--- vanilla/js/main.js
+++ vanilla/js/main.js
@@ -1,53 +1,54 @@
-const apiURL = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
-const newQuoteBtn = document.getElementById('newQuoteBtn');
-const twitterURL = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp,javascript&text=';
+(function() {
+  const apiURL = 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1';
+  const newQuoteBtn = document.getElementById('newQuoteBtn');
+  const twitterURL = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp,javascript&text=';
 
-const decodeHtmlEntity = str => {
-  return str.replace(/&#(\d+);/g, function (match, dec) {
-    return String.fromCharCode(dec);
-  });
-};
+  const decodeHtmlEntity = str => {
+    return str.replace(/&#(\d+);/g, function (match, dec) {
+      return String.fromCharCode(dec);
+    });
+  };
 
-const removeHTMLTags = str => str.replace(/<\/?[^>]+>/gi, '');
+  const removeHTMLTags = str => str.replace(/<\/?[^>]+>/gi, '');
 
-const changeTweet = (quote, author) => {
-  document.querySelector('.share .btn').href = twitterURL + encodeURIComponent('"' + quote.trim() + '" ' + author);
-};
+  const changeTweet = (quote, author) => {
+    document.querySelector('.share .btn').href = twitterURL + encodeURIComponent('"' + quote.trim() + '" ' + author);
+  };
 
-const makeRequest = (type, url, callback) => {
-  const req = new XMLHttpRequest();
-  let response = '';
+  const makeRequest = (type, url, callback) => {
+    const req = new XMLHttpRequest();
+    let response = '';
 
-  req.open(type, url, true);
+    req.open(type, url, true);
 
-  req.onreadystatechange = () => {
-    if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
+    req.onreadystatechange = () => {
+      if (req.readyState === XMLHttpRequest.DONE && req.status === 200) {
 
-      callback(req.responseText);
+        callback(req.responseText);
 
+      }
     }
+    req.send(null);
+  };
+
+  const setNewQuote = data => {
+    const json = JSON.parse(data);
+    const newQuote = decodeHtmlEntity(removeHTMLTags(json[0].content.trim()))
+    const newAuthor = json[0].title.trim();
+
+    document.querySelector('.quote p').innerHTML = newQuote;
+    document.querySelector('.citation span').innerHTML = newAuthor;
+
+    changeTweet(newQuote, newAuthor);
   }
-  req.send(null);
-};
 
-const setNewQuote = data => {
-  const json = JSON.parse(data);
-  const newQuote = decodeHtmlEntity(removeHTMLTags(json[0].content.trim()))
-  const newAuthor = json[0].title.trim();
+  newQuoteBtn.addEventListener('click', function () {
 
-  document.querySelector('.quote p').innerHTML = newQuote;
-  document.querySelector('.citation span').innerHTML = newAuthor;
+    makeRequest('GET', apiURL, setNewQuote);
 
-  changeTweet(newQuote, newAuthor);
-}
+  }, false);
 
-newQuoteBtn.addEventListener('click', function () {
-
+  // run for the first time
   makeRequest('GET', apiURL, setNewQuote);
 
-}, false);
-
-// run for the first time
-makeRequest('GET', apiURL, setNewQuote);
-
-
+})();
